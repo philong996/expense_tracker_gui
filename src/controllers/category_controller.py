804e5a59571e2c7c43ec0b58ba53e368bca177bb
Khanh Phạm <phamkhanh294@gmail.com>
2024-12-22 15:06:30 +0700

    update test and category controller

--- src/controllers/category_controller.py
+++ src/controllers/category_controller.py
@@ -1,14 +1,13 @@
 from src.models.schema import Category
 from .base import BaseController
+from sqlalchemy.exc import SQLAlchemyError
+from src.controllers.base import BaseController
 
-
-
-
-class CategoryController  (BaseController):
+class CategoryController(BaseController):
     """Controller to manage User-related operations (CRUD)"""
-    def Create_Category(self, category_name, description, role='category'):
+    def create_category(self, category_name, description):
         "Create category method"
-        new_category = Category (category_name=category_name, description=description, role=role)
+        new_category = Category (category_name=category_name, description=description)
         try:
             self.db_session.add(new_category)
             self.db_session.commit()
@@ -17,15 +16,27 @@ class CategoryController  (BaseController):
             self.db_session.rollback()
             raise e
 
-    def get_Category(self, catrgory_id):
-        "Get user method"
+    def get_category_by_id(self, catrgory_id):
+        "Get category method"
         return self.db_session.query(Category).get(catrgory_id)
 
-    def delete_Category(self, category_id):
-        """Delete user method"""
-        user = self.get_user(category_id)
-        if user:
-            self.db_session.delete(Category)
+    def delete_category(self, category_id):
+        """Delete category method"""
+        category = self.get_category_by_id(category_id)
+        if category:
+            self.db_session.delete(category)
             self.db_session.commit()
-            
-    #def expenses 
+    def update_category(self, category_id, category_name =None, description=None):
+        category_update= self.get_category_by_id(category_id)
+        try:
+            if not category_update:
+                raise Exception(f"Category with ID{category_id} not found")
+            if category_name is not None:
+                category_update.category_name = category_name
+            if description is not None:
+                category_update.description = description
+            self.db_session.commit()
+            return category_update
+        except SQLAlchemyError as e:
+            self.db_session.rollback()
+            raise Exception(f"Failse to update category: {e}")
---
+++ test/test_category_controller.py
@@ -0,0 +1,42 @@
+import unittest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from src.controllers.category_controller import CategoryController
+from base import BaseTestCase
+class TestCategory (BaseTestCase):
+    def setUp(self):
+        """Set up the test database and session."""
+        super().setUp()  # Call base setUp
+        self.controller = CategoryController(self.session)
+        
+    def test_create_category (self):
+        category = self.controller.create_category(
+            category_name = "name 1",
+            description = "des 2"
+            )
+        self.assertEqual(category.category_name, "name 1")
+        self.assertEqual(category.description, "des 2")
+        
+    def test_get_category(self): 
+       category = self.controller.get_category_by_id (10)
+       self.assertEqual(category.category_name, "name 1")
+       
+    def test_delete_category(self):
+        delete_category = self.controller.create_category(
+            category_name="delete_name",
+            description="delete_des"
+            )
+        self.controller.delete_category(delete_category.category_id)
+        self.assertIsNone(self.controller.get_category_by_id(delete_category.category_id))
+    
+    def test_update_category(self):
+        update_category = self.controller.update_category(category_id=10, category_name="name3", description="des3")
+        self.assertEqual(update_category.category_id, 10)
+        self.assertEqual(update_category.category_name, "name3")
+        self.assertEqual(update_category.description, "des3")  
+        
+
+if __name__ == "__main__":
+    unittest.main()      
+        
